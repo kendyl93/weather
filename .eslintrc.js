69eda5cfac112b8763154197314562d08bdb2ca5
Pawel Stanecki <p.stanecki93@gmail.com>
2020-04-15 22:07:39 +0200

    Added pretrier rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,11 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  parser: "babel-eslint",
-  extends: ["airbnb", "prettier", "prettier/react"],
+  parser: 'babel-eslint',
+  extends: ['airbnb', 'prettier', 'prettier/react'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaFeatures: {
@@ -15,18 +15,18 @@ module.exports = {
       modules: true,
     },
     ecmaVersion: 2018,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react", "prettier"],
+  plugins: ['react', 'prettier'],
   rules: {
-    "prettier/prettier": "error",
-    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
-    "react/forbid-prop-types": [0, { forbid: ["any"] }],
-    "react/prop-types": 0,
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/forbid-prop-types': [0, { forbid: ['any'] }],
+    'react/prop-types': 0,
   },
   env: {
     jest: true,
     browser: true,
     node: true,
   },
-}
+};
